62250e6c179dfe53c214ec584f95ab4fdaac1644
Mona Temchin <monatemchin@gmail.com>
2018-05-16 21:52:43 -0400

    Added tech descriptions for projects, cosmetic styling

--- src/Mona.js
+++ src/Mona.js
@@ -6,6 +6,9 @@ class Mona extends Component {
   render() {
     return (<div className="Mona">
       <header className="Mona-header">
+        <br></br>
+        <br></br>
+        <br></br>
         <h1 className="Mona-title">Mona Temchin, Fullstack Developer, React JS, SQL, etc.</h1>
         <br></br>
         <img src="/Mona.jpg" className="Mona-pic" alt="Mona"/>
@@ -24,21 +27,31 @@ class Mona extends Component {
             <div className="card">
               <a href="https://fabias-fabulous-fairytales.herokuapp.com/"><img src="/Fabia.jpeg" height="150" width="150" className="Fabia-pic" alt="Fabia"/></a>
               <br></br>
-              <p>Fabia's Fabulous Fairytales</p>
-            </div>
+            <h5>Fabia's Fabulous Fairytales</h5>
+              <br></br>
+            <h6>Front end technologies:</h6>
+            <p className="Tech">React, Javascript (JSX), Mobile Responsiveness, Ajax, Bootstrap-4, Node, Yarn, Heroku, .circleci, CSS, HTML, Git, Github, Mac CLI</p>
+            <h6>Back end tech:</h6>
+            <p className="Tech">Postgres, SQL, CORS, Express, Knex, Node, Heroku, .circleci, Git, Github, Mac CLI</p>
+          </div>
           </div>
           <div className="col-sm-4">
             <div className="card">
               <a href="https://still-ravine-87799.herokuapp.com/"><img src="/yinyang.png" height="150" width="150" className="Fabia-pic" alt="Fabia"/></a>
               <br></br>
-              <p>Chinese Herbal Formulas</p>
+            <h5>Chinese Herbal Formulas</h5>
+          <br></br>
+          <h6>Technologies:</h6>
+          <p className="Tech">Javascript, Postgres, Restful Web Services and Routes, CRUD, JSON, Mobile Responsiveness, Knex, Express, EJS, Morgan, JS Promises and Function Chaining, Bootstrap-4, Node, Yarn, Heroku, .circleci,  CSS, HTML</p>
             </div>
           </div>
           <div className="col-sm-4">
             <div className="card">
               <a href="https://monatem.github.io/q1-project-mvp/"><img src="/Chimera.jpg" height="150" width="150" className="Fabia-pic" alt="Fabia"/></a>
               <br></br>
-              <p>Monsters from Mythology and Folklore</p>
+            <h5>Monsters from Mythology and Folklore</h5>
+            <h6>Technologies:</h6>
+            <p className="Tech">Javascript, Materialize CSS, Mobile Responsiveness, DOM manipulation, CSS, HTML, Git, Github, Mac CLI, node</p>
             </div>
           </div>
         </div>
